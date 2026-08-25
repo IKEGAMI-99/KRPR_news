a2a729e139eb4643596bc11fb5d2e2a626108f63
IKEGAMI-99 <hato.jp@gmail.com>
2026-08-25 20:31:45 +0900

    feat: enable v0.2 API-free live repository

--- app/src/main/java/com/ikegami99/krprnews/MainActivity.kt
+++ app/src/main/java/com/ikegami99/krprnews/MainActivity.kt
@@ -3,11 +3,12 @@ package com.ikegami99.krprnews
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.ikegami99.krprnews.data.ApiFreeNewsRepository
 import com.ikegami99.krprnews.ui.KiraparaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { KiraparaApp() }
+        setContent { KiraparaApp(repository = ApiFreeNewsRepository) }
     }
 }
