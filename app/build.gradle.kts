9b3ba3bb99c503d6aab800b130c85220f6cf54ac
IKEGAMI-99 <hato.jp@gmail.com>
2026-08-26 05:27:06 +0900

    chore: bump Kirapara News to 0.3.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ikegami99.krprnews"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.3.3"
+        versionCode = 11
+        versionName = "0.3.4"
 
         ndk {
             abiFilters += "arm64-v8a"
