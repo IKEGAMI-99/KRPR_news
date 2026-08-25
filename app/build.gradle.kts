b7b93b3448ef685d9f6d05fdef04a8849dc49c0f
IKEGAMI-99 <hato.jp@gmail.com>
2026-08-25 21:25:53 +0900

    chore: bump version to 0.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ikegami99.krprnews"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.2.2"
+        versionCode = 5
+        versionName = "0.2.3"
     }
 
     signingConfigs {
