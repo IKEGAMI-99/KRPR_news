59b4f9c5d798baf7907dbec3e8091cc11cceacc7
IKEGAMI-99 <hato.jp@gmail.com>
2026-08-28 11:42:53 +0900

    chore: refresh PWA shell for Weibo image fallback

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kirapara-pwa-shell-v32';
+const CACHE_NAME = 'kirapara-pwa-shell-v33';
 const APP_SHELL = [
   './',
   './index.html',
@@ -16,6 +16,7 @@ const APP_SHELL = [
   './analytics-config.js',
   './analytics-track.js',
   './app.js',
+  './weibo-image-fallback.js',
   './share.js',
   './menu.js',
   './menu-install.js',
